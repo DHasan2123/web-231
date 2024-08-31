39a87145e629f9f7a4597ca7f1e12e86e36fb7b4
DHasan2123 <dderany@ymail.com>
2024-08-30 21:48:29 -0500

    Add files via upload

---
+++ week-3/project2-4/project02-04.js
@@ -0,0 +1,67 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-04
+
+      Application to calculate the cost of a restaurant order plus tax
+      Author: Dua Hasan
+      Date: 08/27/24
+
+      Filename: project02-04.js
+ */
+//constants for prices and sales tax
+const CHICKEN_PRICE = 10.95;
+const HALIBUT_PRICE = 13.95;
+const BURGER_PRICE = 9.95;
+const SALMON_PRICE = 18.95;
+const SALAD_PRICE = 7.95;
+const SALES_TAX = 0.07;
+
+//Event handlers for checkboxes
+document.getElementById("chicken").addEventListener("click", calcTotal);
+document.getElementById('halibut').addEventListener('click', calcTotal);
+document.getElementById('burger').addEventListener('click', calcTotal);
+document.getElementById('salmon').addEventListener('click', calcTotal);
+document.getElementById('salad').addEventListener('click', calcTotal);
+
+
+//Function to calculate total cost
+function calcTotal() {
+  // Initialize cost variable
+  let cost = 0;
+
+  //Get the checked status of each item
+  let buyChicken = document.getElementById("chicken").checked;
+  let buyHalibut = document.getElementById("halibut").checked;
+  let buyBurger = document.getElementById("burger").checked;
+  let buySalmon = document.getElementById("salmon").checked;
+  let buySalad = document.getElementById("salad").checked;
+
+
+  //Calculate total cost based on checked items
+  cost += buyChicken ? CHICKEN_PRICE : 0;
+  cost += buyHalibut ? HALIBUT_PRICE : 0;
+  cost += buyBurger ? BURGER_PRICE : 0;
+  cost += buySalmon ? SALMON_PRICE : 0;
+  cost += buySalad ? SALAD_PRICE : 0;
+
+
+
+
+  //Set the food total display
+  document.getElementById('foodTotal').innerHTML = formatCurrency(cost);
+
+  // Calculate and display tax
+  const tax = cost * SALES_TAX;
+  document.getElementById('foodTax').innerHTML = formatCurrency(tax);
+
+  // Calculate and display total bill
+
+  const totalCost = cost + tax;
+  document.getElementById('totalBill').innerHTML = formatCurrency(totalCost);
+
+}
+
+// Function to display a numeric value as a text string in the format $##.##
+function formatCurrency(value) {
+  return "$" + value.toFixed(2);
+}
